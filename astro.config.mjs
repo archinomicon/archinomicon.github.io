2689712a0dbad7f630c28ee3e04574cc0014697d
german <german.khokhlov@gmail.com>
2025-06-26 10:10:43 +0300

    astro added

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Archinomicon',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/archinomicon',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'The Book',
+          autogenerate: { directory: 'book' },
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: 'The Book', slug: 'book/introduction' },
+          // ],
+        },
+        // {
+        //   label: 'Blog',
+        //   autogenerate: { directory: 'blog' },
+        // },
+      ],
+    }),
+  ],
+});
---
+++ src/content/docs/book/4_summary.md
@@ -0,0 +1,10 @@
+---
+title: 4. Summary
+description: Summary
+---
+
+Summary
+
+## Список литературы 
+1. The TOGAF® Standard, https://www.opengroup.org/togaf
+2. Domain-Driven Design
