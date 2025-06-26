91e5fedb23b6631f1e23dbcc8188fffe5c8f8915
german <german.khokhlov@gmail.com>
2025-06-26 15:24:24 +0300

    text

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
         {
           icon: 'github',
           label: 'GitHub',
-          href: 'https://github.com/archinomicon',
+          href: 'https://github.com/archinomicon/archinomicon.github.io',
         },
       ],
       sidebar: [
